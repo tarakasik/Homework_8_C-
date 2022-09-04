f1c8f6d71303073416945952ec3425a999d9f42a
Stanislav Volokhov <lamantina123@gmail.com>
2022-09-04 18:49:21 +0400

    make #62

--- Task#62/Program.cs
+++ Task#62/Program.cs
@@ -5,16 +5,60 @@
 // 11 16 15 06
 // 10 09 08 07
 
-int[,] matrix = new int[4,4];
 
-void FillArray(int[,] matrix)
+int[,] matrix = FillWithSpiral(Prompt("Input size of matrix: "));
+PrintMatrix(matrix);
+
+void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = Next(1, 16);
-            Console.Write("{0}\t",matrix[i, j]);
+            System.Console.Write($"{matrix[i, j]}\t");
         }
+        System.Console.WriteLine();
+
     }
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] FillWithSpiral(int size)
+{
+    int elements = size * size;
+    int countElement = 0;
+    int begin = 0;
+    int[,] matrix = new int[size, size];
+    while (countElement < elements)
+    {
+        for (int i = begin; i < size; i++)
+        {
+            countElement++;
+            matrix[begin, i] = countElement;
+        }
+        for (int i = begin + 1; i < size; i++)
+        {
+            countElement++;
+            matrix[i, size - 1] = countElement;
+        }
+        for (int i = size - 2; i >= begin; i--)
+        {
+            countElement++;
+            matrix[size - 1, i] = countElement;
+        }
+        for (int i = size - 2; i >= begin + 1; i--)
+        {
+            countElement++;
+            matrix[i, begin] = countElement;
+        }
+        size -= 1;
+        begin += 1;
+    }
+    return matrix;
 }
